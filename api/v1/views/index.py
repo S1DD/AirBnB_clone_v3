4068fae8d2c42bbe0759292737575daaa2debfbb
Thabiso <thabisomolefe515@gmail.com>
2025-02-17 14:53:31 -0800

    initial commit

---
+++ api/v1/views/index.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+index
+"""
+
+from flask import jsonify
+from api.v1.views import app_views
+from models import storage
+
+
+@app.route("/status", methods=['GET'], strict_slashes=False)
+def status():
+     """
+     status route
+     :return: response with json
+     """
+     data = {
+         "status" : "OK"
+         }
+
+     res = jsonify(data)
+     return res
+
+@app.route("/stats", methods=['GET'], strict_slashes=False)
+ def stats():
+     """
+     stats of all objs route
+     :return: json of all objs
+     """
+     data = {
+         "amenities": storage.count("Amenity")
+         "cities": storage.count("City")
+         "places": storage.count("Place")
+         "reviews": storage.count("Review")
+         "states": storage.count("State")
+         "users": storage.count("User")
+         }
+
+     res = jsonify(data)
+     res.status_code = 200
+
+     return res
