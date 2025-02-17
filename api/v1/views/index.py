846216525f12205483708db4562abd6f5454bf36
Thabiso <thabisomolefe515@gmail.com>
2025-02-17 15:38:33 -0800

    Fixed indentation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,33 +10,34 @@ from models import storage
 
 @app.route("/status", methods=['GET'], strict_slashes=False)
 def status():
-     """
-     status route
-     :return: response with json
-     """
-     data = {
-         "status" : "OK"
-         }
+    """
+    status route
+    :return: response with json
+    """
+    data = {
+        "status": "OK"
+    }
 
-     res = jsonify(data)
-     return res
+    res = jsonify(data)
+    return res
+
 
 @app.route("/stats", methods=['GET'], strict_slashes=False)
- def stats():
-     """
-     stats of all objs route
-     :return: json of all objs
-     """
-     data = {
-         "amenities": storage.count("Amenity")
-         "cities": storage.count("City")
-         "places": storage.count("Place")
-         "reviews": storage.count("Review")
-         "states": storage.count("State")
-         "users": storage.count("User")
-         }
+def stats():
+    """
+        stats of all objs route
+        :return: json of all objs
+        """
+    data = {
+        "amenities": storage.count("Amenity")
+        "cities": storage.count("City")
+        "places": storage.count("Place")
+        "reviews": storage.count("Review")
+        "states": storage.count("State")
+        "users": storage.count("User")
+    }
 
-     res = jsonify(data)
-     res.status_code = 200
+    res = jsonify(data)
+    res.status_code = 200
 
-     return res
+    return res
